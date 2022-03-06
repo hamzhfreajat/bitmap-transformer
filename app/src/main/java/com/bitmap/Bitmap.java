ff860d69e63ffad80e97aa661afa82e3ee95b834
Hamzh Freijat <hamzhg2016@gmail.com>
2022-03-06 23:05:37 +0300

    Solved

--- app/src/main/java/com/bitmap/Bitmap.java
+++ app/src/main/java/com/bitmap/Bitmap.java
@@ -9,25 +9,19 @@ import java.io.IOException;
 public class Bitmap {
 
 
-    private String transform_type;
+
     private int height;
     private int width;
     private BufferedImage img = null;
 
 
-
-    public String getTransform_type() {
-        return transform_type;
+    public int getHeight() {
+        return height;
     }
 
-    public void setHeight(int height) {
-        this.height = height;
+    public int getWidth() {
+        return width;
     }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
 
     public void readImage(String input_path ) {
 
